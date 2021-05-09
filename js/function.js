c7f8740fbbf2cec2214da9cc02aa5d0088605e47
BoxInCoded <matas.mts@icloud.com>
2021-05-09 18:45:53 +0300

    Added function.js to call list from data js

---
+++ js/function.js
@@ -0,0 +1,35 @@
+"use strict";
+
+function getData(data) {
+
+    let HTML = '';
+    let sectionBody = document.querySelector('.first__text');
+
+    if (Array.isArray(data)) {
+
+        // -----------
+        for (let i = 0; i < data.length; i++) {
+
+            HTML += getList(data[i]);
+        }
+
+        return sectionBody.innerHTML = HTML;
+        // -----------
+
+    } else {
+        return console.error('Tai ne masyvas!');
+    }
+}
+
+function getList(list) {
+
+    console.log(list.Text.first_text);
+
+    let HTML = `<h1>${list.Text.first_text}</h1>`;
+
+    return HTML;
+}
+
+
+//----------- !!!!!!!
+getData(feed);
