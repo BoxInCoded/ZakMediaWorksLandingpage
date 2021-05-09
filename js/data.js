d25aa40bd15eada8a70b1cf7ee90455aae16d1f8
BoxInCoded <matas.mts@icloud.com>
2021-05-09 14:02:34 +0300

    Added JS Data file

---
+++ js/data.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const feed = [{
+    Text: {
+        first_text: 'Video projektai nuo idėjos iki finalo',
+        text_block: 'Lorem ipsum dolor sit, amet consectetur adipisicing?',
+        title_block: 'Lorem ipsum dolor',
+        bigger_text_block: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex earum tenetur odit nemo id architecto laborum iure error, aliquid nisi . Ex earum tenetur odit nemo id architecto laborum iure error, . Ex earum tenetur odit nemo id architecto laborum iure error',
+    },
+    Button: {
+        first_button: 'Daugiau',
+        second_button: 'Daugiau',
+        third_button: 'Daugiau',
+        send_message_button: 'Send'
+    },
+    Testimonial: {
+        client_image: [
+            'pexelsphoto952237.png',
+        ],
+        client_text: '„Viskas labai organizuotai ir kokybiškai. Geri video montavimo \n' +
+            'sugebėjimai, vaizdo raiška aukščiausios kokybės. \n' +
+            'Kokybės ir kainos santykis labai geras.“',
+        client_name: 'Conor',
+        client_last_name: 'McGregor'
+    },
+    Contact: {
+        contact_title: 'Contact Us',
+        contact_number: '66666666666',
+        contact_email: 'info@zakmediaworks.eu'
+    },
+    CopyRight: {
+        copyright_text: 'zak media works'
+    }
+}]
