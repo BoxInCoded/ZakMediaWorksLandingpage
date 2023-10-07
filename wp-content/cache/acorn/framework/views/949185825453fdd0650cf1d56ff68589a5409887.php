12f7b2630ff5f7c738e480a0a571dd9be240bcd6
matas12 <matas.mts@icloud.com>
2023-10-07 17:46:53 +0300

    Prideta veikianti tema

---
+++ wp-content/cache/acorn/framework/views/949185825453fdd0650cf1d56ff68589a5409887.php
@@ -0,0 +1,4 @@
+<div class="page-header">
+  <h1><?php echo $title; ?></h1>
+</div>
+<?php /**PATH /shared/httpd/zakmediaworks/zakmediaworks/wp-content/themes/videography/resources/views/partials/page-header.blade.php ENDPATH**/ ?>
---
+++ wp-content/cache/acorn/framework/views/c665c0fe1c6a33077160e53194e39e2c35254aa7.php
@@ -0,0 +1,2 @@
+<?php (dynamic_sidebar('sidebar-primary')); ?>
+<?php /**PATH /shared/httpd/zakmediaworks/zakmediaworks/wp-content/themes/videography/resources/views/sections/sidebar.blade.php ENDPATH**/ ?>
